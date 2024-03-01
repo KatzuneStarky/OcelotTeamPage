682992716d8509fee26337ee9169fa2ff9b40f8f
KatzuneStarky <aramirez_17@alu.uabcs.mx>
2024-03-01 13:29:36 -0700

    fix fix

--- app/(root)/company/_components/team-hero.tsx
+++ app/(root)/company/_components/team-hero.tsx
@@ -7,7 +7,7 @@ export const TeamHero = () => {
                 <div className="flex flex-col lg:flex-row justify-center items-center py-12 mx-4 md:mx-6 ">
                     <div className="lg:w-1/2 flex flex-col justify-center items-center lg:items-start">
                         <h1 className="text-3xl lg:text-4xl xl:text-5xl font-bold text-gray-800 text-center lg:text-left">Meet our professionals</h1>
-                        <p className="text-base leading-normal text-gray-600 mt-4 md:w-8/12 lg:w-11/12 text-center lg:text-left">A good idiom for kids is "It&apos;s raining cats and dogs." Kids know what both cats and dogs are from an early age</p>
+                        <p className="text-base leading-normal text-gray-600 mt-4 md:w-8/12 lg:w-11/12 text-center lg:text-left">A good idiom for kids is &quot;It&apos;s raining cats and dogs.&quot; Kids know what both cats and dogs are from an early age</p>
                         <div className="w-full flex justify-center lg:justify-start">
                             <button className="bg-indigo-700 text-base font-medium text-white rounded text-center py-4 px-9 mt-8 lg:mt-12 hover:bg-indigo-600 leading-4 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 w-full sm:w-auto">View All</button>
                         </div>
